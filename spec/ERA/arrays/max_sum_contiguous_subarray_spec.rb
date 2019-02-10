1f6f37878af19806bdc59c8bd0c28db2414b4a04
Nikolay Konovalov <konovalov.nk@gmail.com>
2019-02-11 00:37:01 +0300

    Change code style a bit

--- spec/ERA/arrays/max_sum_contiguous_subarray_spec.rb
+++ spec/ERA/arrays/max_sum_contiguous_subarray_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe 'MaxSumContiguousSubarray' do
   subject { ERA::Arrays::MaxSumContiguousSubarray.new }
 
   it 'returns 6 for [-2, 1, -3, 4, -1, 2, 1, -5, 4]' do
-    expect(subject.solution([-2, 1, -3, 4, -1, 2, 1, -5, 4])).to be 6
+    expect(subject.solution([-2, 1, -3, 4, -1, 2, 1, -5, 4])).to be(6)
   end
 
   describe 'general cases' do
@@ -21,11 +21,11 @@ RSpec.describe 'MaxSumContiguousSubarray' do
 
   describe 'edge cases' do
     it 'works with left edge' do
-      expect(subject.solution([100, -2, -3, 1])).to be 100
+      expect(subject.solution([100, -2, -3, 1])).to be(100)
     end
 
     it 'works with right edge' do
-      expect(subject.solution([1, -3, -2, 100])).to be 100
+      expect(subject.solution([1, -3, -2, 100])).to be(100)
     end
 
     it 'returns nil for empty array' do

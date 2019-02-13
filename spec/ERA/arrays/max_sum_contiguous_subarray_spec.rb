54a2d796551175de1d74ff640a4a5143bc190443
Nikolay Konovalov <konovalov.nk@gmail.com>
2019-02-13 04:08:16 +0300

    Change `to be` to `to eq`

--- spec/ERA/arrays/max_sum_contiguous_subarray_spec.rb
+++ spec/ERA/arrays/max_sum_contiguous_subarray_spec.rb
@@ -2,34 +2,34 @@ RSpec.describe 'MaxSumContiguousSubarray' do
   subject { ERA::Arrays::MaxSumContiguousSubarray.new }
 
   it 'returns 6 for [-2, 1, -3, 4, -1, 2, 1, -5, 4]' do
-    expect(subject.solution([-2, 1, -3, 4, -1, 2, 1, -5, 4])).to be(6)
+    expect(subject.solution([-2, 1, -3, 4, -1, 2, 1, -5, 4])).to eq(6)
   end
 
   describe 'general cases' do
     it 'works with positive-only values' do
-      expect(subject.solution([1, 2, 3, 2, 1])).to be(9)
+      expect(subject.solution([1, 2, 3, 2, 1])).to eq(9)
     end
 
     it 'works with negative-only values' do
-      expect(subject.solution([-1, -2, -3, -2, -1])).to be(-1)
+      expect(subject.solution([-1, -2, -3, -2, -1])).to eq(-1)
     end
 
     it 'works with both negative and positive values' do
-      expect(subject.solution([1, 2, -3, 2, 1])).to be(3)
+      expect(subject.solution([1, 2, -3, 2, 1])).to eq(3)
     end
   end
 
   describe 'edge cases' do
     it 'works with left edge' do
-      expect(subject.solution([100, -2, -3, 1])).to be(100)
+      expect(subject.solution([100, -2, -3, 1])).to eq(100)
     end
 
     it 'works with right edge' do
-      expect(subject.solution([1, -3, -2, 100])).to be(100)
+      expect(subject.solution([1, -3, -2, 100])).to eq(100)
     end
 
     it 'returns nil for empty array' do
-      expect(subject.solution([])).to be(nil)
+      expect(subject.solution([])).to eq(nil)
     end
   end
 
--- spec/ERA/stacks/rain_water_trapped_spec.rb
+++ spec/ERA/stacks/rain_water_trapped_spec.rb
@@ -2,26 +2,26 @@ RSpec.describe 'RainWaterTrapped' do
   subject { ERA::Stacks::RainWaterTrapped.new }
 
   it 'returns 6 for [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]' do
-    expect(subject.solution([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])).to be(6)
+    expect(subject.solution([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])).to eq(6)
   end
 
   describe 'general cases' do
     it 'works with positive-only values' do
-      expect(subject.solution([3, 2, 1, 2, 3])).to be(4)
+      expect(subject.solution([3, 2, 1, 2, 3])).to eq(4)
     end
 
     it 'works with negative-only values' do
-      expect(subject.solution([-1, -2, -3, -2, -3])).to be(1)
+      expect(subject.solution([-1, -2, -3, -2, -3])).to eq(1)
     end
 
     it 'works with both negative and positive values' do
-      expect(subject.solution([1, 2, -3, 2, 1])).to be(5)
+      expect(subject.solution([1, 2, -3, 2, 1])).to eq(5)
     end
   end
 
   describe 'edge cases' do
     it 'returns 0 for empty array' do
-      expect(subject.solution([])).to be(0)
+      expect(subject.solution([])).to eq(0)
     end
   end
 

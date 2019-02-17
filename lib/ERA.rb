d4785623078baaaa2e3c19126834fc4de48a6223
Nikolay Konovalov <konovalov.nk@gmail.com>
2019-02-17 04:26:33 +0300

    Use recursive library include

--- lib/ERA.rb
+++ lib/ERA.rb
@@ -1,9 +1,4 @@
-require 'ERA/version'
-require 'ERA/arrays/max_sum_contiguous_subarray'
-require 'ERA/arrays/max_distance'
-require 'ERA/arrays/maxspprod'
-require 'ERA/arrays/repeat_and_missing_number_array'
-require 'ERA/stacks/rain_water_trapped'
+Dir["#{File.dirname(__FILE__)}/ERA/**/*.rb"].each(&method(:require))
 
 module ERA
   class Error < StandardError; end
